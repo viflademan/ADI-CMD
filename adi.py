631daf0716e95b55c176fb281a5b26067a6e1d3b
dlademan <david@lademan.net>
2018-11-08 04:32:47 -0500

    1.3.0

--- adi.py
+++ adi.py
@@ -1,5 +1,17 @@
-import menu, manage, p, cfg #adi modules
+import sys, menu, manage, p, cfg #adi modules
 
 p.makeDirs() #make directories on startup
 manage.makePs(False) #make reference files
-menu.main() #go to main menu
+
+if len(sys.argv) == 1:
+	menu.main() #go to main menu
+elif sys.argv[1] == "-ia":
+	zips = p.gZips()
+	toInstall = p.gToInstall()
+	for file in zips:
+		toInstall.append(file[:-4])
+	p.sToInstall(toInstall)
+	manage.install()
+else:
+	print("Invalid arguments.")
+	input("	  ")
--- manage.py
+++ manage.py
@@ -48,34 +48,35 @@ def install(): #install zips
 		zFile = zipfile.ZipFile(path) #open the asset's zip file
 		zList = zFile.infolist() #list of files in zip file
 		importList = [] #make an empty array 
-		i = 0
 		for member in zFile.namelist(): #for every member in zipfile
 			if "Manifest" in member or "Supplement" in member: #skip manifest and supplement
 				continue
 				
 			filename = os.path.basename(member)
-			if zList[i].filename[:8] == "Content/": #chop off Content/ from member location
-				localDir = zList[i].filename[8:]
-			elif zList[i].filename[:11] == "My Library/": #chop off My Library/ from member location
-				localDir = zList[i].filename[11:]
+			
+			if member[:8] == "Content/": #chop off Content/ from member location
+				localDir = member[8:]
+			elif member[:11] == "My Library/": #chop off My Library/ from member location
+				localDir = member[11:]
 			else:
-				localDir = zList[i].filename
-			i += 1
+				localDir = member
 			if not filename:
 				if not os.path.exists(path):
 					os.makedirs(path)
 				continue
-			if os.path.exists(path):
-				overwrite += 1
+			#doesn't work lol not fixing now
+			#if os.path.exists(path):
+			#	overwrite += 1
 	
 			# copy file (taken from zipfile's extract)
 			source = zFile.open(member)
-			dest = cfg.dazDir / localDir
-			if not os.path.exists(dest.parent): #if folder does not exist
-				os.makedirs(dest.parent) #create folder
-			with zFile.open(member) as source, open(dest, 'wb') as dest:
-				shutil.copyfileobj(source, dest)
-			
+			dest = os.path.join(str(cfg.dazDir), localDir)
+			pathFolder = dest.replace(filename, "")
+			if not os.path.exists(pathFolder): #if folder does not exist
+				os.makedirs(pathFolder) #create folder
+			with zFile.open(member) as source, open(dest, 'wb') as out:
+				shutil.copyfileobj(source, out)
+				
 			# add dsx metadata file to list of assets to be imported in daz
 			if cfg.dsaImport and member.endswith('.dsx'):
 				importList.append(filename)
@@ -100,9 +101,7 @@ def install(): #install zips
 				#print("\t   Could not remove source " + file + ".zip")
 				
 		dsa.writeImport(importList) #write import.dsa
-		
-		
-		
+
 		installed.append(file) #add asset to list of installed assets
 		installed = list(set(installed)) #remove duplicate assets
 		installed = sorted(installed, key=str.lower) #sort assets alphabetically
